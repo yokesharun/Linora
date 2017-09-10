1a8ae7ef37e49505a5a3aa04e5101081850011d0
Sid <sid@appoets.com>
2017-09-11 03:23:28 +0530

    Link Validation and Popup

--- resources/views/receive.blade.php
+++ resources/views/receive.blade.php
@@ -77,26 +77,34 @@
 
                 <div id="qr_code">
                     {!! QrCode::size(250)->generate($code); !!}
+
+                    <p>Note : Scan this QR code in your Linora Android App</p>
                 </div>
 
-                <p>Note : Scan this QR code in your Linora Android App</p>
-
-                <a href="" class="result"></a>
-
+                <div id="result">
+                    
+                </div>
             </div>
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript">
             (function worker() {
-              $.get('/show',{ code : "{{$code}}" }, function(data) {
+                $.get('/show',{ code : "{{$code}}" }, function(data) {
+                    if(data.status == 'USED' && data.value != ''){
+                        $('#qr_code').fadeOut(100);
 
-                if(data.status == 'USED' && data.value != ''){
-                    $('#qr_code').fadeOut(100);
-                    $('.result').attr('href',data.value).html(data.value).fadeIn(200);
-                }
+                        re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
 
-                setTimeout(worker, 5000);
-              });
+                        if (!re.test(data.value)) {
+                            $('#result').html(data.value);
+                        } else {
+                            $('#result').html('<a href="'+data.value+'" target="_blank">'+data.value+'</a>').fadeIn(200);
+                            $('#result a')[0].click();
+                        }
+                    } else {
+                        var timer = setTimeout(worker, 1000);
+                    }
+                });
             })();
         </script>
     </body>
